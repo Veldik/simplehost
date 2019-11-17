cef8dce52248b9ea08023e71ce8d726fdb827b0b
Jakub Velička <37436112+Veldik@users.noreply.github.com>
2019-11-17 15:33:56 +0100

    Creating MySQL user and database, domain is var

--- generate_user.sh
+++ generate_user.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 nick=$1 # Nick is first atribut of command
 pass=$2 # Password is second atribut of command
+mysqlrootpass=HESLO # MySQL root password
+domain=example.com # Define domain
 # Making home dirrectory
 webDir="/home/$nick/public_html"
 sudo mkdir -p $webDir
@@ -14,8 +16,8 @@ sudo chown $nick:$nick $webDir
 # Setting up hosting
 sitesAvailable="/etc/apache2/sites-available/$nick.conf"
 echo "<VirtualHost *:80>
-  ServerAdmin webmaster@example.com
-  ServerName $nick.example.com
+  ServerAdmin webmaster@${domain}
+  ServerName ${nick}.${domain}
   DocumentRoot $webDir
   ErrorLog /var/log/apache2/error.log
   CustomLog /var/log/apache2/access.log combined
@@ -25,3 +27,10 @@ echo "<VirtualHost *:80>
 </VirtualHost>" > $sitesAvailable
 sudo a2ensite $nick.conf
 sudo systemctl restart apache2
+# Creating MySQL user and database
+mysql -uroot -p${mysqlrootpass}<<ENDMYSQL
+CREATE DATABASE ${nick};
+CREATE USER '${nick}'@'localhost' IDENTIFIED BY '${pass}';
+GRANT ALL PRIVILEGES ON ${nick}.* TO '${nick}'@'localhost';
+FLUSH PRIVILEGES;
+ENDMYSQL
